1f5762180b8e69dd08c8699bc414a531ff7e9879
shivashankarareddy <ssr2tech@gmail.com>
2024-02-21 20:58:25 +0530

    another update

--- script.js
+++ script.js
@@ -12,7 +12,26 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-
+function  validateDate(){
+    let inputDate = new Date(document.querySelector("#date"));
+    let today= new Date();
+    console.log(schedule_recall);
+    document.querySelector("#date").setAttribute("min", formatDate(today));
+    
+  
+    if(inputDate <today){
+      document.querySelector("#date").value="";
+      alert("plz select date form current date onwords." );
+    }
+  }
+  
+  function formatDate(){
+    let year= date.getFullYear();
+    let month= (date.getMonth()+1).toString();
+    let date= date.getDate().toString();
+    return `${year}-${month}-${date}`;
+  
+  }
 
   
 
